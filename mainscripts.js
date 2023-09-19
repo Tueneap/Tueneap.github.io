f2bf8e38509611d00db0cd465bcb0c68eb93bdc0
Tueneap <107796335+Tueneap@users.noreply.github.com>
2023-09-19 22:33:39 +0200

    add js and further updates

---
+++ mainscripts.js
@@ -0,0 +1,19 @@
+const listOfSounds =
+[
+	"a",
+	"b",
+	"c",
+];
+
+function populatelist()
+{
+	var list = document.getElementById("soundList");
+	var i;
+	listOfSounds.forEach(function(item) {
+	  const listItem = document.createElement("li");
+	  listItem.textContent = item;
+	  list.appendChild(listItem);
+	});
+}
+
+populatelist();
